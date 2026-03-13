892066a969be4144ecb826ab19ffdb8ed5fa4599
David Ospina <J_do_o13@hotmail.com>
2026-03-13 12:35:18 +0100

    Model example
    This is a example for model.ui use (to transfer data beetwen controller and view

--- src/main/java/com/megof/springboot/shop/springboot_shop/controllers/UserController.java
+++ src/main/java/com/megof/springboot/shop/springboot_shop/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.megof.springboot.shop.springboot_shop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 //handler maneja peticiones
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title", "Details");
+        model.addAttribute("name", "Megof");
+        model.addAttribute("lastname", "Timer");
         return "details";
     }
     
