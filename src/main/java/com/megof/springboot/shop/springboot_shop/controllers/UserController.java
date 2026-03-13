228c90e40a4391af4a3fecebea9a6d997d717b98
David Ospina <J_do_o13@hotmail.com>
2026-03-13 12:43:53 +0100

    Map example
    This is a Map.ui example to transfer data beetwen View an Controller

--- src/main/java/com/megof/springboot/shop/springboot_shop/controllers/UserController.java
+++ src/main/java/com/megof/springboot/shop/springboot_shop/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.megof.springboot.shop.springboot_shop.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 //handler maneja peticiones
     @GetMapping("/details")
-    public String details(Model model) {
+    public String details(Model model, Map<String, Object> map) {
         model.addAttribute("title", "Details");
         model.addAttribute("name", "Megof");
         model.addAttribute("lastname", "Timer");
+
+        map.put("date", "13/03/2026");
+        map.put("time", "12:39");
         return "details";
     }
     
