e42c4b48be6725835e67a77ba320da5ae0001dc6
David Ospina <J_do_o13@hotmail.com>
2026-03-18 12:39:35 +0100

    table each
    a for each exmple with thymeleaf is added

--- src/main/java/com/megof/springboot/shop/springboot_shop/controllers/UserController.java
+++ src/main/java/com/megof/springboot/shop/springboot_shop/controllers/UserController.java
@@ -1,12 +1,18 @@
 package com.megof.springboot.shop.springboot_shop.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class UserController {
@@ -28,6 +34,19 @@ public class UserController {
 
         map.put("user", user.toString());
         return "details";
+    }
+    
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        User user = new User("Megof", "Timer", 0);
+        User user1 = new User("David", "Ospina", 3);
+        User user2 = new User("Juan", "Ortega", 9);
+
+       List<User> users = Arrays.asList(user, user1, user2); 
+
+        model.addAttribute("title", "List");
+        model.addAttribute("users", users);
+        return "list";
     }
     
 }
